87053e0c33adfa9c98c5e4768e6f782ee9d83e98
Joe Mulcahy <joseph.r.mulcahy@gmail.com>
2025-05-15 12:03:06 +0100

    moved logic from drums_pad_module to drum_pad_app.py

--- drum_pad_app.py
+++ drum_pad_app.py
@@ -1,12 +1,17 @@
 from PyQt6.QtCore import QObject
 from PyQt6.QtWidgets import QWidget, QGridLayout
 
+from app_enums.wave_form_enum import WaveForm
 from drum_pads.drum_pads_module import DrumPadModule
 from file_manager.audio_file_manager import FileManager
 from globals_controls.globals_settings_module import GlobalControls
 from midi.app_midi import AppMidi
 from sample_editor.sample_view import SampleViewer
+from sound_engine.AudioChannel import AudioChannel
+from sound_engine.AudioVoice import AudioVoice
+from sound_engine.SoundEngine import SoundEngine
 from sound_engine.SynthVoice import SynthVoice
+import utility.music_notes as mn
 
 
 class DrumPadApp(QWidget):
@@ -15,22 +20,38 @@ class DrumPadApp(QWidget):
         self.__selected_pad_index = 0
 
         self.__file_manager = FileManager()
+        self.__sound_engine = SoundEngine()
 
-        self.__app_layout = QGridLayout()
-        self.__pads = DrumPadModule()
+        self.__pad_voices_list = []  # list of voices associated with pad
+        self.__audio_channels_list = []  # list of audio channel associated with pad
+
+        # initialise sound engine and audio channels
+        self.__pad_voices_list = self.__create_pad_voices()
+        self.__audio_channels_list = self.__create_audio_channels()
+        self.__add_channels_to_engine()
+
+        self.__pads_module = DrumPadModule()
         self.__global_controls = GlobalControls()
 
-        test_sample = self.__pads.pad_voices[89].voice_data
-
+        test_sample = self.__pad_voices_list[0].voice_data
         self.__sample_view = SampleViewer(test_sample)
+        self.highlight_selected(self.__selected_pad_index)
 
-        self.__app_layout.addWidget(self.__pads, 0, 0, 4, 4)
+        self.__app_layout = QGridLayout()
+        self.__app_layout.addWidget(self.__pads_module, 0, 0, 4, 4)
         self.__app_layout.addWidget(self.__global_controls, 0, 4, 1, 3)
         self.__app_layout.addWidget(self.__sample_view, 1, 4, 3, 3)
 
         self.setLayout(self.__app_layout)
 
         self.__app_midi = AppMidi()
+
+        self.__pads_module.pad_matrix_list[self.__selected_pad_index].select()
+        self.__sound_engine.play()  # active sound engine
+
+        #########################################################
+        ##  Listeners
+        #########################################################
 
         # populate global_controls.device.combobox with available devices
         self.__global_controls.device_combo_box.addItems(self.__app_midi.ports_list)
@@ -44,49 +65,112 @@ class DrumPadApp(QWidget):
         self.__app_midi.mm_signal_note_off.connect(lambda on, note: self.midi_trigger_note_off(on, note))
         self.__app_midi.mm_signal_cc.connect(lambda cc, value: self.midi_trigger_cc(cc, value))
 
-        # listener for pads
-        for i, btn in enumerate(self.__pads.pad_list):
+        # listener for pressing pads
+        for i, btn in enumerate(self.__pads_module.pad_matrix_list):
             button = btn.button
+            button.clicked.connect(lambda clicked, index=i: self.highlight_selected(index))
             button.pressed.connect(lambda index=i: self.update_editor_waveform(index))
+            button.pressed.connect(lambda index=i: self.trigger_pad(index))
 
         # listener for wave_viewer load sample button
         self.__sample_view.load_sample_button.clicked.connect(lambda: self.__load_sample())
 
+        # listeners for wave_viewer start, end, stretch and pitch
+        self.__sample_view.start_pos_dial.clicked.connect()
+        self.__sample_view.end_pos_dial.clicked.connect()
+        self.__sample_view.pitch_dial.clicked.connect()
+        self.__sample_view.stretch_dial.clicked.connect()
+
         # signal listeners
-        self.__file_manager.files_loaded_signal.connect(lambda l: self.load_voice_to_pad(l))
+        self.__file_manager.files_loaded_signal.connect(lambda l: self.load_voice(l))
 
+    ###############################################
+    # create default pad voices
+    ###############################################
+    def __create_pad_voices(self):
+        temp_list = []
+        for i in range(len(mn.NOTE_FREQUENCIES_LIST)):
+            freq = mn.NOTE_FREQUENCIES_LIST[i]
+            voice = SynthVoice(WaveForm.TRIANGLE, float(freq), 0.2, 1.0, 44100)
+            temp_list.append(voice)
+
+        return temp_list
+
+    ###############################################
+    # create audio channels for each voice
+    ###############################################
+    def __create_audio_channels(self):
+        temp_list = []
+        for i in range(len(self.__pad_voices_list)):
+            audio_channel = AudioChannel(i, self.__pad_voices_list[i], volume=0.5, pan=0.5)
+            temp_list.append(audio_channel)
+
+        return temp_list
+
+    ###############################################
+    # add each audio channel to the sound engine
+    ###############################################
+    def __add_channels_to_engine(self):
+        for channel in self.__audio_channels_list:
+            self.__sound_engine.add_channel(channel)
+
+    ###############################################
+    # highlight pad and update currently_selected_pad_index
+    ###############################################
+    def highlight_selected(self, index):
+        for pad in self.__pads_module.pad_matrix_list:
+            pad.unselect()
+
+        self.__pads_module.pad_matrix_list[index].select()
+        self.__pads_module.currently_selected_pad_index = index
+        self.__selected_pad_index = index
+
+    ###############################################
+    # play the sound sample associated with pad
+    ###############################################
+    def trigger_pad(self, index):
+        self.__audio_channels_list[index].trigger()
+
+    ###############################################
+    # load sequential pads with voices
+    ###############################################
     def __load_samples_in_sequence(self):
-        self.__file_manager.get_files_using_explorer()
+        self.__file_manager.get_files_using_explorer("directory")
 
+    ###############################################
+    # loads single pad with voice
+    ###############################################
     def __load_sample(self):
-        index = self.__pads.current_selected_pad
-        print(f'pad index for load: {index}')
         self.__file_manager.get_files_using_explorer("single")
 
-    def load_voice_to_pad(self, file_list: list):
-        print(f'test: {str(file_list[0])}')
-        print(f'selected pad: {self.__selected_pad_index}')
-
+    def load_voice(self, file_list: list):
         if len(file_list) == 1:
-            self.__pads.load_voice_to_pad(str(file_list[0]), self.__selected_pad_index)
+            self.load_voice_to_pad(str(file_list[0]), self.__selected_pad_index)
             self.update_editor_waveform(self.__selected_pad_index)
         else:
             for i in range(len(file_list)):
-                self.__pads.load_voice_to_pad(str(file_list[i]), (self.__selected_pad_index + i) % len(self.__pads.pad_list))
+                self.load_voice_to_pad(str(file_list[i]),
+                                       (self.__selected_pad_index + i) % len(self.__pads_module.pad_matrix_list))
 
             self.update_editor_waveform(self.__selected_pad_index)
 
+    def load_voice_to_pad(self, file, pad_index):
+        voice = AudioVoice(file)
+        self.__pad_voices_list[pad_index] = voice
+        audio_channel = AudioChannel(pad_index, voice, volume=1.0, pan=0.5)
+        self.__audio_channels_list[pad_index] = audio_channel
+        self.__sound_engine.update_audio_channels(self.__audio_channels_list)
 
     def update_editor_waveform(self, index):
-        self.__sample_view.waveform_widget.sample_data = self.__pads.pad_voices[index].voice_data
         print(f'editor waveform: {index}')
+        self.__sample_view.waveform_widget.sample_data = self.__pad_voices_list[index].voice_data
         self.__selected_pad_index = index
         self.repaint()
 
     def midi_trigger_note_on(self, on, note, velocity):
         index = (note - 36) + 32
-        self.__pads.trigger_pad(index)
-        self.__pads.highlight_selected(index)
+        self.__pads_module.trigger_pad(index)
+        self.__pads_module.highlight_selected(index)
 
     def midi_trigger_note_off(self, off, note):
         pass
@@ -99,3 +183,7 @@ class DrumPadApp(QWidget):
     def __change_port(self, index):
         self.__app_midi.select_midi_port(index)
         self.__app_midi.open_inport()
+
+    @property
+    def sound_engine(self):
+        return self.__sound_engine
--- drum_pads/drum_pads_module.py
+++ drum_pads/drum_pads_module.py
@@ -1,38 +1,23 @@
 from PyQt6.QtCore import Qt
 from PyQt6.QtWidgets import QWidget, QGridLayout, QGroupBox, QPushButton
 
-from app_enums.wave_form_enum import WaveForm
-from drum_pads.drum_pad import DrumPad
 import utility.music_notes as mn
-from sound_engine.AudioChannel import AudioChannel
-from sound_engine.AudioVoice import AudioVoice
-from sound_engine.SoundEngine import SoundEngine
-from sound_engine.SynthVoice import SynthVoice
+from drum_pads.drum_pad import DrumPad
 
 
 class DrumPadModule(QWidget):
     def __init__(self):
         super().__init__()
         self.__midi_notes_dict: dict = mn.MIDI_NOTE_NUMBER_TO_MUSIC_NOTE_DICT
-        self.__bank_index = 3
-        self.__pads_list = []
+        self.__bank_index = 0
+        self.__pad_matrix_list = []
         self.__currently_selected_pad_index = 0
         self.__bank_buttons_list = []
-        self.__pad_voices_list = []
-        self.__audio_channels_list = []
 
         self.__bank_btn_default_style = ""
         self.__bank_btn_selected_style = "QPushButton { background-color: #999999}"
 
-        # initialise sound engine and audio channels
-        self.__sound_engine = SoundEngine()
-        self.__pad_voices_list = self.__create_pad_voices()
-        self.__audio_channels_list = self.__create_audio_channels()
-        self.__add_channels_to_engine()
-
-        self.test_list_voice()
-
-        # banks initialisation
+        # initialise bank buttons and add to bank_buttons_layout
         bank_buttons_layout = QGridLayout()
         for i in range(0, 6):
             btn = QPushButton(f'{i + 1}')
@@ -43,17 +28,16 @@ class DrumPadModule(QWidget):
         self.__pads_layout = QGridLayout()
         self.__pads_layout.setSpacing(1)
 
-        # create pads list
+        # create drum pads and add to pad_matrix_list
         for i in range(len(mn.MIDI_NOTE_NUMBERS_LIST)):
             midi_note = mn.MIDI_NOTE_NUMBERS_LIST[i]
             music_note = mn.MUSIC_NOTES_LIST[i]
             pad = DrumPad(midi_note, music_note)
-            self.__pads_list.append(pad)
+            self.__pad_matrix_list.append(pad)
 
-        # display current pads
-        self.__update_visible_pads()
+        self.__update_visible_pads()  # display current bank of pads and add to self.__pads_layout
 
-        group_box = QGroupBox('Pads')
+        group_box = QGroupBox('Pads')  # group box for pads matrix
 
         module_layout = QGridLayout()
         module_layout.addLayout(bank_buttons_layout, 0, 0, )
@@ -65,31 +49,29 @@ class DrumPadModule(QWidget):
         main_layout.addWidget(group_box)
         self.setLayout(main_layout)
 
-        self.__set_bank_index(3)
-        self.__sound_engine.play()  # active sound engine
+        self.__update_bank_of_pads(self.__bank_index)   # initial bank to display
 
         # listeners for bank buttons
         for i in range(len(self.__bank_buttons_list)):
             button = self.__bank_buttons_list[i]
-            button.clicked.connect(lambda clicked, index=i: self.__set_bank_index(index))
+            button.clicked.connect(lambda clicked, index=i: self.__update_bank_of_pads(index))
 
-        # listener for pads
-        for i, btn in enumerate(self.__pads_list):
-            button = btn.button
-            button.clicked.connect(lambda clicked, index=i: self.highlight_selected(index))
-            button.pressed.connect(lambda index=i: self.trigger_pad(index))
+    #     # listener for pads
+    #     for i, btn in enumerate(self.__pad_matrix_list):
+    #         button = btn.button
+    #         button.clicked.connect(lambda clicked, index=i: self.highlight_selected(index))
+    #
+    # def highlight_selected(self, index):
+    #     for pad in self.__pad_matrix_list:
+    #         pad.unselect()
+    #
+    #     self.__pad_matrix_list[index].select()
+    #     self.__currently_selected_pad_index = index
 
-    def highlight_selected(self, index):
-        for pad in self.__pads_list:
-            pad.unselect()
-
-        self.__pads_list[index].select()
-        self.__currently_selected_pad_index = index
-
-    def trigger_pad(self, index):
-        self.__audio_channels_list[index].trigger()
-
-    def __set_bank_index(self, index):
+    #############################################
+    ## update which bank of pads are visible
+    #############################################
+    def __update_bank_of_pads(self, index):
         for btn in self.__bank_buttons_list:
             btn.setStyleSheet(self.__bank_btn_default_style)
 
@@ -98,12 +80,12 @@ class DrumPadModule(QWidget):
         self.__bank_index = index
         self.__clear_grid_layout()
         self.__update_visible_pads()
-        self.highlight_selected(self.__currently_selected_pad_index)
+
 
     def __update_visible_pads(self):
         visible_pads = []
         for i in range(0, 16):
-            visible_pads.append(self.__pads_list[self.__bank_index * 16 + i])
+            visible_pads.append(self.__pad_matrix_list[self.__bank_index * 16 + i])
 
         for row in range(0, 4):
             for col in range(0, 4):
@@ -115,67 +97,25 @@ class DrumPadModule(QWidget):
             item = self.__pads_layout.takeAt(0)
             widget = item.widget()
             if widget is not None:
-                widget.setParent(None)  # Removes the widget from the GUI
+                widget.setParent(None)
             else:
-                # If it's a layout or spacer, you might need to handle it differently
                 sub_layout = item.layout()
                 if sub_layout is not None:
                     self.clear_grid_layout(sub_layout)  # Recursively clear nested layouts
-
-    def __create_pad_voices(self):
-        temp_list = []
-        for i in range(len(mn.NOTE_FREQUENCIES_LIST)):
-            freq = mn.NOTE_FREQUENCIES_LIST[i]
-            voice = SynthVoice(WaveForm.TRIANGLE, float(freq), 0.2, 1.0, 44100)
-            temp_list.append(voice)
-
-        return temp_list
-
-    def __create_audio_channels(self):
-        temp_list = []
-        for i in range(len(self.__pad_voices_list)):
-            audio_channel = AudioChannel(i, self.__pad_voices_list[i], volume=0.5, pan=0.5)
-            temp_list.append(audio_channel)
-
-        return temp_list
-
-    def load_voice_to_pad(self, file, pad_index):
-        voice = AudioVoice(file)
-        self.__pad_voices_list[pad_index] = voice
-        audio_channel = AudioChannel(pad_index, voice, volume=1.0, pan=0.5)
-        self.__audio_channels_list[pad_index] = audio_channel
-        print('------------------------')
-        self.test_list_voice()
-        self.__sound_engine.update_audio_channels(self.__audio_channels_list)
-
-    def __add_channels_to_engine(self):
-        for channel in self.__audio_channels_list:
-            self.__sound_engine.add_channel(channel)
 
     @property
     def drum_pads_module(self):
         return self
 
     @property
-    def pad_list(self):
-        return self.__pads_list
+    def pad_matrix_list(self):
+        return self.__pad_matrix_list
+
 
     @property
-    def pad_voices(self):
-        return self.__pad_voices_list
-
-    @property
-    def current_selected_pad(self):
+    def current_selected_pad_index(self):
         return self.__currently_selected_pad_index
 
     @property
-    def current_selected_voice(self):
-        return self.__pad_voices_list[self.__currently_selected_pad_index]
-
-    def test_list_voice(self):
-        for voice in self.__pad_voices_list:
-            print(voice)
-
-    @property
-    def sound_engine(self):
-        return self.__sound_engine
+    def bank_index(self):
+        return self.__bank_index
